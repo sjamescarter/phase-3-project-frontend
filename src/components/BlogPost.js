0ec4bbee4cd724014aaedb5d9daf24e166b49b25
sjamescarter <stephenjamescarter@gmail.com>
2023-04-26 16:26:40 -0500

    create BlogPost.js and Comment.js

---
+++ src/components/BlogPost.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+function BlogPost() {
+    const [blogPost, setBlogPost] = useState()
+    let params = useParams();
+    console.log(params)
+    useEffect(() => {
+        fetch('http://localhost:9292/posts/' + params.id)
+        .then(r => r.json())
+        .then(data => setBlogPost(data))
+    }, []);
+
+    if(!blogPost) {
+        return (
+            <p>Loading...</p>
+        )
+    }
+
+    console.log(blogPost.comments)
+
+    return (
+        <div>
+            <h1>{blogPost.title}</h1>
+            <small>{blogPost.author}</small>
+            <p>{blogPost.body}</p>
+            {blogPost.comments.map((comment) => {
+                return <Comment key={comment.id} comment={comment} />
+            })}
+        </div>
+    );
+}
+
+export default BlogPost
