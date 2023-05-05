03836f535eeec024e72b3bbcf634fc8d4fa277e1
sjamescarter <stephenjamescarter@gmail.com>
2023-05-05 13:07:58 -0500

    update comment style

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -4,7 +4,7 @@ function Comment({ comment, onDelete, login }) {
     return (
         <div className="comment">
             <h4>
-                {comment.name} says
+                {comment.name} • <small>{comment.created_at.slice(0, 10)}</small>
                 {login ? <button onClick={() => onDelete(comment.id)}>🗑</button> : null}
             </h4>
             <p>{comment.comment}</p>
